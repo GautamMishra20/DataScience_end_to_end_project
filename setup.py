0b4026a859d7dda62802937588858ebe5df1c5d0
Gautam Mishra <g20autammishra1234@gmail.com>
2026-06-25 11:34:00 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->list[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+    return requirements
+    
+
+setup(
+    name='DataScience_project',
+    version='0.0.1',
+    author='Gautam Mishra',
+    author_email='g20autammishra1234@gmail',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
