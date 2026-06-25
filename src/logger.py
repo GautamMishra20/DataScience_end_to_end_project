cf5d3e12a8d968f55642a9bfd26e88600f80a2f8
Gautam Mishra <g20autammishra1234@gmail.com>
2026-06-25 16:20:10 +0530

    starting the EDA part

--- src/logger.py
+++ src/logger.py
@@ -12,6 +12,3 @@ logging.basicConfig(
     format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
     level=logging.INFO
 )   
-
-# if __name__ == '__main__':
-#     logging.info('Logged successfully!!')
